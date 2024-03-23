880d45601f48f52172dacf8c244fad873082a01b
wa7-A-6ash1 <dev.yneiucffanejk@gmail.com>
2024-03-23 21:07:53 +0900

    [change] EasyGLTFLoaderの変更に伴う型定義の変更

--- src/types/three.ts
+++ src/types/three.ts
@@ -5,11 +5,10 @@ export type MtlAndObjFiles = [MtlPath, ObjPath];
 export type GlbPath = string;
 
 export type Size = number;
-export type X = number;
-export type Y = number;
-export type Z = number;
+export type Position = [number, number, number];
+export type Rotation = [number, number, number];
 
-export type GLTFFile = [GlbPath, Size, X, Y, Z];
+export type GLTFFile = [GlbPath, Size, Position, Rotation];
 
 export type SunParameters = {
     inclination: number;
