eff53bc4496e8866cb9a10e421695ebe5e360f58
yukyan <yukyanjpn@gmail.com>
2024-06-02 10:42:36 +0900

    assetsの自動検出範囲を拡張 https://ja.vitejs.dev/guide/assets#importing-asset-as-url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    target: 'esnext'
-  },
+    build: {
+        target: 'esnext'
+    },
+    assetsInclude: ['**/*.glb'],
 });
